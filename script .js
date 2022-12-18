e4de805f36a4104c63f3dfb20767d19655ca3a88
Mariem Khaled <mariamkhaled709@gmail.com>
2022-12-18 04:48:46 +0300

    final

--- script .js
+++ script .js
@@ -1,69 +1,59 @@
-let arrayData =[
-    {
-      "day": "mon",
-      "amount": 17.45
-    },
-    {
-      "day": "tue",
-      "amount": 34.91
-    },
-    {
-      "day": "wed",
-      "amount": 52.36
-    },
-    {
-      "day": "thu",
-      "amount": 31.07
-    },
-    {
-      "day": "fri",
-      "amount": 23.39
-    },
-    {
-      "day": "sat",
-      "amount": 43.28
-    },
-    {
-      "day": "sun",
-      "amount": 25.48
-    }
-  ]
-  
- const bar =document.querySelectorAll(".bar "); 
 
-  bar.forEach((element,index)=>
-{  
+ fetch("data.json")
+ .then(response => response.json())
+ .then(data => show(data))
+ .catch(function (error) {
+  console.error("error"); })
+ 
+function show(data) {
+ 
+  const bar =document.querySelectorAll(".bar "); 
+  
+
+   bar.forEach((element,index)=>
+ {    let height=data[index].amount *2;
+      element.style.height=`${height}px`;
+      element.style.width=`35px`;
+      element.style.backgroundColor="hsl(10, 79%, 65%)";
+      element.style.borderRadius=`5px`
+      
+ })   
    
-    let height=arrayData[index].amount*2;
-     element.classList.add("bars");
-     element.style.height=`${height}px`;
-     element.style.width=`30px`;
-     element.style.backgroundColor="hsl(10, 79%, 65%)";
-     element.style.borderRadius=`5px`
+   const weekday=[];
+   data.forEach(element=>{
+       weekday.push(element.day);
+       console.log(weekday)
+   });
+
+   let date= new Date().getDay()
+    console.log(date)
+   let currentDay=weekday[date]
+    currentDay =="mon" ? currentDay= "sun" :currentDay= weekday[date-1] ;
+    console.log(currentDay)
+
+    
    
   
-})     
-  const weekday=[];
-  arrayData.forEach(element=>{
-      weekday.push(element.day,);
-  });
+ bar.forEach((element,index)=>
+ { const dayValue=element.className=weekday[index]
+   if (dayValue == currentDay) {
+     element.style.backgroundColor=" hsl(186, 34%, 60%)";
+   }
+ })     
 
-  let date= new Date();
-  let currentDay= weekday[date.getDay()-1];
-
-bar.forEach((element,index)=>
-{ const dayValue=element.className=weekday[index]
-  if (dayValue == currentDay) {
-    element.style.backgroundColor=" hsl(186, 34%, 60%)"
-  }
-})     
-
-bar.forEach((element,index) =>{ 
-  let value=arrayData[index].amount;
-  element.addEventListener("mouseover",
-  function () {
-    element.innerHTML=`<div class ="tag" > $ ${value} </div>` 
-  }
-  )
-  
-})
+ bar.forEach((element,index) =>{ 
+   let value=data[index].amount;
+   element.addEventListener("mouseover",
+      function () {
+        const tag=`<div id="tag"> $ ${value} </div>` ;
+        element.insertAdjacentHTML("afterbegin",tag); 
+        
+      }
+   )
+   element.addEventListener("mouseout",function () {
+      element.innerHTML="";
+    
+     
+   })
+ })
+}
